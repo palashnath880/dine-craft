3a8719416b570f59db1ac42327d19c104d2957c3
Palash Nath <palashnath880@gmail.com>
2025-07-24 18:44:41 +0600

    contact route updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,94 +1,86 @@
+import PageHeader from "@/components/shared/PageHeader";
 import React from "react";
+import { FaEnvelope, FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
 
 export default function page() {
   return (
     <>
-      <div className="container-fluid bg-light py-6 my-6 mt-0">
-        <div className="container text-center animated bounceInDown">
-          <h1 className="display-1 mb-4">Contact</h1>
-          <ol className="breadcrumb justify-content-center mb-0 animated bounceInDown">
-            <li className="breadcrumb-item">
-              <a href="#">Home</a>
-            </li>
-            <li className="breadcrumb-item">
-              <a href="#">Pages</a>
-            </li>
-            <li className="breadcrumb-item text-dark" aria-current="page">
-              Contact
-            </li>
-          </ol>
-        </div>
-      </div>
+      <PageHeader name="Contact" />
 
-      <div
-        className="container-fluid contact py-6 wow bounceInUp"
-        data-wow-delay="0.1s"
-      >
+      <section className="py-16">
         <div className="container">
-          <div className="p-5 bg-light rounded contact-form">
-            <div className="row g-4">
-              <div className="col-12">
-                <small className="d-inline-block fw-bold text-dark text-uppercase bg-light border border-primary rounded-pill px-4 py-1 mb-3">
-                  Get in touch
-                </small>
-                <h1 className="display-5 mb-0">Contact Us For Any Queries!</h1>
-              </div>
-              <div className="col-md-6 col-lg-7">
-                <p className="mb-4">
-                  {`The contact form is currently inactive. Get a functional and
-                  working contact form with Ajax & PHP in a few minutes. Just
-                  copy and paste the files, add a little code and you're done.`}
-                  <a href="https://htmlcodex.com/contact-form">Download Now</a>.
-                </p>
-                <form>
+          <div className="flex flex-col gap-16 bg-primary/5 px-16 py-16 shadow-md rounded-2xl">
+            <div className="flex flex-col items-start gap-5">
+              <small
+                className={`font-semibold uppercase border-2 border-primary rounded-full px-4 py-1 text-secondary`}
+              >
+                Get in touch
+              </small>
+              <h1 className={`font-bold font-playball text-5xl text-secondary`}>
+                Contact Us For Any Queries!
+              </h1>
+            </div>
+
+            <div className="grid flex-1 grid-cols-12 gap-20">
+              <div className="col-span-12 md:col-span-6">
+                <form className="flex flex-col gap-5">
                   <input
+                    required
+                    name="name"
                     type="text"
-                    className="w-100 form-control p-3 mb-4 border-primary bg-light"
+                    className="w-full rounded-full outline-primary py-3 px-6 border border-primary"
                     placeholder="Your Name"
                   />
                   <input
+                    required
+                    name="email"
                     type="email"
-                    className="w-100 form-control p-3 mb-4 border-primary bg-light"
+                    className="w-full rounded-full outline-primary py-3 px-6 border border-primary"
                     placeholder="Enter Your Email"
                   />
                   <textarea
-                    className="w-100 form-control mb-4 p-3 border-primary bg-light"
-                    // rows="4"
-                    //   cols="10"
-
+                    required
+                    name="message"
+                    className="w-full rounded-xl outline-primary py-3 px-6 border border-primary mt-5"
+                    rows={4}
+                    cols={10}
                     placeholder="Your Message"
                   ></textarea>
-                  <button
-                    className="w-100 btn btn-primary form-control p-3 border-primary bg-primary rounded-pill"
-                    type="submit"
-                  >
+                  <button className="btn-primary" type="submit">
                     Submit Now
                   </button>
                 </form>
               </div>
-              <div className="col-md-6 col-lg-5">
-                <div>
-                  <div className="d-inline-flex w-100 border border-primary p-4 rounded mb-4">
-                    <i className="fas fa-map-marker-alt fa-2x text-primary me-4"></i>
-                    <div className="">
-                      <h4>Address</h4>
-                      <p>123 Street, New York, USA</p>
+
+              <div className="col-span-12 md:col-span-6">
+                <div className="flex flex-col gap-5">
+                  <div className="flex items-start gap-5 border border-primary p-4 rounded-md">
+                    <FaMapMarkerAlt className="text-primary text-4xl" />
+                    <div className="flex flex-col gap-1">
+                      <h4 className="text-secondary font-semibold text-lg">
+                        Address
+                      </h4>
+                      <p className="text-gray-500">123 Street, New York, USA</p>
                     </div>
                   </div>
-                  <div className="d-inline-flex w-100 border border-primary p-4 rounded mb-4">
-                    <i className="fas fa-envelope fa-2x text-primary me-4"></i>
-                    <div className="">
-                      <h4>Mail Us</h4>
-                      <p className="mb-2">info@example.com</p>
-                      <p className="mb-0">support@example.com</p>
+                  <div className="flex items-start gap-5 border border-primary p-4 rounded-md">
+                    <FaEnvelope className="text-primary text-4xl" />
+                    <div className="flex flex-col gap-1">
+                      <h4 className="text-secondary font-semibold text-lg">
+                        Mail Us
+                      </h4>
+                      <p className="text-gray-500">info@example.com</p>
+                      <p className="text-gray-500">support@example.com</p>
                     </div>
                   </div>
-                  <div className="d-inline-flex w-100 border border-primary p-4 rounded">
-                    <i className="fa fa-phone-alt fa-2x text-primary me-4"></i>
-                    <div className="">
-                      <h4>Telephone</h4>
-                      <p className="mb-2">(+012) 3456 7890 123</p>
-                      <p className="mb-0">(+704) 5555 0127 296</p>
+                  <div className="flex items-start gap-5 border border-primary p-4 rounded-md">
+                    <FaPhoneAlt className="text-primary text-4xl" />
+                    <div className="flex flex-col gap-1">
+                      <h4 className="text-secondary font-semibold text-lg">
+                        Telephone
+                      </h4>
+                      <p className="text-gray-500">(+012) 3456 7890 123</p>
+                      <p className="text-gray-500">(+704) 5555 0127 296</p>
                     </div>
                   </div>
                 </div>
@@ -96,7 +88,7 @@ export default function page() {
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
