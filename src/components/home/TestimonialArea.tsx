ce58ea4493ceea171b1a0ff109f2027d675765f7
Palash Nath <palashnath880@gmail.com>
2025-07-27 03:26:51 +0600

    eslint error fixed

--- src/components/home/TestimonialArea.tsx
+++ src/components/home/TestimonialArea.tsx
@@ -78,10 +78,10 @@ export default function TestimonialArea() {
               </h5>
 
               <p className="text-secondary/70 mt-10">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
+                {`Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                of type and scrambled it to make a type specimen book.`}
               </p>
             </div>
           </div>
