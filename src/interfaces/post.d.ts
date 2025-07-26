97056e16a9b4350335f50435c7d05f14f61ae36b
Palash Nath <palashnath880@gmail.com>
2025-07-26 23:26:32 +0600

    post interface added

---
+++ src/interfaces/post.d.ts
@@ -0,0 +1,10 @@
+export type Post = {
+  id: string;
+  title: string;
+  description: string;
+  created_at: string;
+  category: string;
+  comments_count: number;
+  img_url: string;
+  slug: string;
+};
