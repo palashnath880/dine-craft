9af40cf8235752176b2e7f33ea5bb359489a6eb0
Palash Nath <palashnath880@gmail.com>
2025-07-26 23:27:21 +0600

    BlogArea component updated

--- src/components/home/BlogArea.tsx
+++ src/components/home/BlogArea.tsx
@@ -2,20 +2,27 @@ import React from "react";
 import BlogItem from "../shared/BlogItem";
 import SectionHeading from "./SectionHeading";
 import Link from "next/link";
+import { getPosts } from "@/lib/fetchers";
 
-export default function BlogArea() {
+export default async function BlogArea() {
+  const posts = await getPosts();
+  const filteredPosts = posts
+    .sort((a, b) => b.comments_count - a.comments_count)
+    .slice(0, 6);
+
   return (
     <section className="py-16">
       <div className="container">
         <SectionHeading title="Our Blog" subtitle="Be First Who Read News" />
 
         <div className="grid grid-cols-12 gap-x-8 gap-y-10 mt-16">
-          {[...Array(6)].map((_, index) => (
-            <BlogItem key={index} />
+          {filteredPosts.map((item) => (
+            <BlogItem key={item.id} {...item} />
           ))}
         </div>
+
         <div className="flex items-center justify-center mt-10">
-          <Link href={"/blog"} className="btn-primary">
+          <Link href={"/blogs"} className="btn-primary">
             Show More
           </Link>
         </div>
