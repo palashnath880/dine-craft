13b6a09f5f19869fbba09de5f56d39f6b9ae990a
Palash Nath <palashnath880@gmail.com>
2025-07-27 22:34:33 +0600

    Header responsive layout updated

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import SearchModal from "./SearchModal";
+import { CiMenuFries } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
+import clsx from "clsx";
 
 export default function Header() {
+  // state
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const menus = [
     {
       label: "Home",
@@ -31,63 +39,71 @@ export default function Header() {
   ];
 
   return (
-    <header>
-      <div className="container mx-auto">
+    <header className="max-lg:relative">
+      <div className="container">
         <nav className="flex items-center justify-between py-4">
           {/* branding */}
-          <Link href={"/"} className="w-3/12">
-            <h1 className="text-primary font-bold !text-4xl font-playball">
+          <Link href={"/"} className="lg:w-3/12">
+            <h1 className="text-primary font-bold max-sm:text-3xl !text-4xl font-playball">
               Dine<span className="text-secondary">Craft</span>
             </h1>
           </Link>
 
           {/* menu button */}
-          <button className="hidden py-2 px-3" type="button">
-            <span className="fa fa-bars text-primary"></span>
+          <button
+            className="lg:hidden text-2xl"
+            type="button"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? (
+              <IoMdClose strokeWidth={1.5} />
+            ) : (
+              <CiMenuFries strokeWidth={1.5} />
+            )}
           </button>
 
           {/* menus start */}
-          <div className="flex gap-7 flex-1 justify-center">
-            {menus.map(({ href, label }, index) => (
-              <Link
-                href={href}
-                key={index}
-                className="font-semibold text-gray-500 hover:text-primary transition duration-300"
-              >
-                {label}
-              </Link>
-            ))}
+          <div
+            className={clsx(
+              "max-lg:absolute max-lg:top-full max-lg:left-0 max-lg:w-full max-lg:bg-white max-lg:shadow-md",
+              {
+                "max-lg:opacity-100 max-lg:scale-y-100": isOpen,
+                "max-lg:opacity-0 max-lg:scale-y-0": !isOpen,
+              },
+              "transition-all duration-300 max-lg:origin-top"
+            )}
+          >
+            <ul
+              className={clsx(
+                "flex gap-7 flex-1 justify-center",
+                "max-lg:gap-4 max-lg:flex-col max-lg:px-5 max-lg:py-5 max-lg:bg-primary/5"
+              )}
+            >
+              {menus.map(({ href, label }, index) => (
+                <li key={index}>
+                  <Link
+                    href={href}
+                    className="font-semibold text-gray-500 hover:text-primary transition duration-300"
+                  >
+                    {label}
+                  </Link>
+                </li>
+              ))}
 
-            {/* <div className="nav-item dropdown">
-                <a
-                  href="#"
-                  className="nav-link dropdown-toggle"
-                  data-bs-toggle="dropdown"
+              <li className="lg:hidden flex items-center gap-5 justify-start mt-5">
+                <SearchModal />
+                <Link
+                  href="/booking"
+                  className="py-2.5 px-6 bg-primary text-secondary font-semibold rounded-full"
                 >
-                  Pages
-                </a>
-                <div className="dropdown-menu bg-light">
-                  <a href="book.html" className="dropdown-item">
-                    Booking
-                  </a>
-                  <a href="blog.html" className="dropdown-item">
-                    Our Blog
-                  </a>
-                  <a href="team.html" className="dropdown-item">
-                    Our Team
-                  </a>
-                  <a href="testimonial.html" className="dropdown-item">
-                    Testimonial
-                  </a>
-                  <a href="404.html" className="dropdown-item">
-                    404 Page
-                  </a>
-                </div>
-              </div> */}
+                  Book Now
+                </Link>
+              </li>
+            </ul>
           </div>
           {/* menus end */}
 
-          <div className="flex items-center justify-end-safe gap-3 w-3/12">
+          <div className="flex items-center justify-end-safe gap-3 max-lg:hidden w-3/12">
             {/* search modal*/}
             <SearchModal />
 
