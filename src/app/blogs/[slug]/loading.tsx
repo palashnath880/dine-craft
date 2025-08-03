5c5a2b2eede853d72b0b01e7300a1bec78d6eb16
Palash Nath <palashnath880@gmail.com>
2025-08-03 19:39:15 +0600

    blog loading component updated

--- src/app/blogs/[slug]/loading.tsx
+++ src/app/blogs/[slug]/loading.tsx
@@ -7,7 +7,7 @@ export default function loading() {
         <div className="bg-primary/5 w-full md:w-5/7 rounded-lg px-5 py-10 shadow h-[40vh] md:h-[50vh] animate-pulse">
           <div className="flex flex-col gap-5">
             <div className="flex flex-col gap-3">
-              <div className="text-4xl font-playball font-semibold text-secondary/90 animate-pulse h-10 animate-pulse"></div>
+              <div className="text-4xl font-playball font-semibold text-secondary/90 h-10 animate-pulse"></div>
               <div className="flex items-center gap-6 text-gray-500">
                 <h6 className="flex items-center gap-2 font-medium "></h6>
                 <span className=" font-medium flex items-center gap-2 max-sm:text-sm"></span>
