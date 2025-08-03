bc5acfb6a2b1fc6d7b37e366d26fd8062bf34756
Palash Nath <palashnath880@gmail.com>
2025-08-03 20:55:58 +0600

    FactArea component updated

--- src/components/home/FactArea.tsx
+++ src/components/home/FactArea.tsx
@@ -1,11 +1,42 @@
 "use client";
 
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaUsersCog } from "react-icons/fa";
 import { FaCheck, FaUsers } from "react-icons/fa6";
 import { IoIosPlay } from "react-icons/io";
+import {
+  animate,
+  motion,
+  useInView,
+  useMotionValue,
+  useTransform,
+} from "motion/react";
 
+// Counter component
+const Counter = ({ count }: { count: number }) => {
+  // states
+  const ref = useRef(null);
+  const value = useMotionValue(0);
+  const rounded = useTransform(() => Math.round(value.get()));
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (!isInView) return;
+
+    const controls = animate(value, count, { duration: 3 });
+
+    return () => controls.stop();
+  }, [count, isInView]);
+
+  return (
+    <motion.h1 ref={ref} className="text-5xl font-playball font-bold">
+      {rounded}
+    </motion.h1>
+  );
+};
+
+// FactArea component
 export default function FactArea() {
   // state
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -38,21 +69,21 @@ export default function FactArea() {
                 <div className="col-span-4 bg-primary rounded-xl pb-10 pt-7">
                   <div className="flex flex-col gap-5 p-4 justify-start items-center text-center">
                     <FaUsers className="text-white text-7xl" />
-                    <h1 className="text-5xl font-playball font-bold">689</h1>
+                    <Counter count={689} />
                     <p className="uppercase font-semibold">Happy Customers</p>
                   </div>
                 </div>
                 <div className="col-span-4 bg-primary rounded-xl pb-10 pt-7">
                   <div className="flex flex-col gap-5 p-4 justify-start items-center text-center">
                     <FaUsersCog className="text-white text-7xl" />
-                    <h1 className="text-5xl font-playball font-bold">107</h1>
+                    <Counter count={107} />
                     <p className="uppercase font-semibold">Expert Chefs</p>
                   </div>
                 </div>
                 <div className="col-span-4 bg-primary rounded-xl pb-10 pt-7">
                   <div className="flex flex-col gap-5 p-4 justify-start items-center text-center">
                     <FaCheck className="text-white text-7xl" />
-                    <h1 className="text-5xl font-playball font-bold">253</h1>
+                    <Counter count={253} />
                     <p className="uppercase font-semibold">Events Complete</p>
                   </div>
                 </div>
@@ -69,13 +100,21 @@ export default function FactArea() {
                   className="w-full h-full object-cover"
                 />
                 <div className="w-full h-full top-0 left-0 absolute grid place-items-center">
-                  <button
+                  <motion.button
                     type="button"
                     className="w-20 h-20 bg-white rounded-full grid place-items-center cursor-pointer"
                     onClick={() => setIsOpen(true)}
+                    initial={{ transform: "scale(1)" }}
+                    animate={{ transform: "scale(1.2)" }}
+                    transition={{
+                      repeat: Infinity,
+                      duration: 1,
+                      repeatType: "mirror",
+                      ease: "easeInOut",
+                    }}
                   >
                     <IoIosPlay className="text-secondary text-5xl" />
-                  </button>
+                  </motion.button>
                 </div>
               </div>
             </div>
@@ -90,7 +129,8 @@ export default function FactArea() {
         }`}
       >
         <div className="w-full h-full grid place-items-center">
-          <div className="modal-body w-[90%] md:w-2xl lg:w-4xl h-0 pb-[56.25%] relative overflow-hidden">
+          {/* pb-[56.25%] */}
+          <div className="modal-body w-[90%] md:w-2xl lg:w-4xl aspect-video relative overflow-hidden">
             <iframe
               className="w-full h-full absolute top-0 left-0"
               src="https://www.youtube.com/embed/GcDXopVBN-k?si=oV9Q3CTAuSXaKcyd"
