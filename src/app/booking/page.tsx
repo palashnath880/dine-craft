cdd32cd241698fcff96359c0455565c2c5db012e
Palash Nath <palashnath880@gmail.com>
2025-09-03 17:58:04 +0600

    booking page updated

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -4,6 +4,7 @@ import { Input, SelectInput } from "@/components/shared/CustomInputs";
 import PageHeader from "@/components/shared/PageHeader";
 import { BookingForm, bookingFormSchema } from "@/schemas/booking.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
 import React from "react";
 import { useForm } from "react-hook-form";
 
@@ -24,7 +25,7 @@ export default function Page() {
 
       <section className="py-16">
         <div className="container">
-          <div className="flex flex-col gap-16 bg-primary/5 max-md:px-5 px-16 py-16 shadow-md rounded-2xl">
+          <div className="flex flex-col gap-16 bg-primary/5 max-md:px-5 px-16 py-16 shadow-md rounded-2xl relative overflow-hidden">
             <div className="flex flex-col items-start gap-5">
               <small
                 className={`font-semibold uppercase border-2 border-primary rounded-full px-4 py-1 text-secondary`}
@@ -38,119 +39,128 @@ export default function Page() {
               </h1>
             </div>
 
-            <div className="flex">
-              <form
-                className="w-full md:w-3/5"
-                onSubmit={handleSubmit(onSubmit)}
-              >
-                <div className="grid grid-cols-12 gap-x-5 gap-y-5 sm:gap-y-7">
-                  <div className="col-span-full sm:col-span-6">
-                    <Input
-                      type="text"
-                      name="name"
-                      control={control}
-                      placeholder="Your Name"
-                    />
-                  </div>
-                  <div className="col-span-full sm:col-span-6">
-                    <Input
-                      type="text"
-                      name="email"
-                      control={control}
-                      placeholder="Your Email"
-                    />
-                  </div>
-                  <div className="col-span-full sm:col-span-6">
-                    <Input
-                      type="text"
-                      name="phone"
-                      control={control}
-                      placeholder="Your Phone"
-                    />
-                  </div>
-                  <div className="col-span-full sm:col-span-6">
-                    <SelectInput
-                      name="eventType"
-                      control={control}
-                      placeholder="Select Event Type"
-                      options={[
-                        {
-                          label: "Wedding",
-                          value: "Wedding",
-                        },
-                        {
-                          label: "Cocktail",
-                          value: "Cocktail",
-                        },
-                        {
-                          label: "Corporate",
-                          value: "Corporate",
-                        },
-                        {
-                          label: "Buffet",
-                          value: "Buffet",
-                        },
-                      ]}
-                    />
-                  </div>
-                  <div className="col-span-full sm:col-span-6">
-                    <Input
-                      type="date"
-                      name="eventDate"
-                      control={control}
-                      placeholder="Event Date"
-                    />
-                  </div>
-                  <div className="col-span-full sm:col-span-6">
-                    <Input
-                      type="time"
-                      name="eventTime"
-                      control={control}
-                      placeholder="Event Time"
-                    />
-                  </div>
-                  <div className="col-span-full sm:col-span-6">
-                    <Input
-                      type="number"
-                      name="noOfGuest"
-                      control={control}
-                      placeholder="Number of Guests"
-                    />
-                  </div>
-                  <div className="col-span-full sm:col-span-6">
-                    <Input
-                      type="text"
-                      name="location"
-                      control={control}
-                      placeholder="Event Location"
-                    />
-                  </div>
+            <div className="flex gap-20">
+              <div className="w-full md:w-3/5">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="grid grid-cols-12 gap-x-5 gap-y-5 sm:gap-y-7">
+                    <div className="col-span-full sm:col-span-6">
+                      <Input
+                        type="text"
+                        name="name"
+                        control={control}
+                        placeholder="Your Name"
+                      />
+                    </div>
+                    <div className="col-span-full sm:col-span-6">
+                      <Input
+                        type="text"
+                        name="email"
+                        control={control}
+                        placeholder="Your Email"
+                      />
+                    </div>
+                    <div className="col-span-full sm:col-span-6">
+                      <Input
+                        type="text"
+                        name="phone"
+                        control={control}
+                        placeholder="Your Phone"
+                      />
+                    </div>
+                    <div className="col-span-full sm:col-span-6">
+                      <SelectInput
+                        name="eventType"
+                        control={control}
+                        placeholder="Select Event Type"
+                        options={[
+                          {
+                            label: "Wedding",
+                            value: "Wedding",
+                          },
+                          {
+                            label: "Cocktail",
+                            value: "Cocktail",
+                          },
+                          {
+                            label: "Corporate",
+                            value: "Corporate",
+                          },
+                          {
+                            label: "Buffet",
+                            value: "Buffet",
+                          },
+                        ]}
+                      />
+                    </div>
+                    <div className="col-span-full sm:col-span-6">
+                      <Input
+                        type="date"
+                        name="eventDate"
+                        control={control}
+                        placeholder="Event Date"
+                      />
+                    </div>
+                    <div className="col-span-full sm:col-span-6">
+                      <Input
+                        type="time"
+                        name="eventTime"
+                        control={control}
+                        placeholder="Event Time"
+                      />
+                    </div>
+                    <div className="col-span-full sm:col-span-6">
+                      <Input
+                        type="number"
+                        name="noOfGuest"
+                        control={control}
+                        placeholder="Number of Guests"
+                      />
+                    </div>
+                    <div className="col-span-full sm:col-span-6">
+                      <Input
+                        type="text"
+                        name="location"
+                        control={control}
+                        placeholder="Event Location"
+                      />
+                    </div>
 
-                  <div className="col-span-12">
-                    <div className="flex flex-col gap-4 max-md:gap-7 items-start">
-                      <div className="flex items-center gap-2">
-                        <input
-                          type="checkbox"
-                          id="terms&condition"
-                          className="w-5 h-5 cursor-pointer"
-                        />
-                        <label
-                          htmlFor="terms&condition"
-                          className="cursor-pointer"
+                    <div className="col-span-12">
+                      <div className="flex flex-col gap-4 max-md:gap-7 items-start">
+                        <div className="flex items-center gap-2">
+                          <input
+                            type="checkbox"
+                            id="terms&condition"
+                            className="w-5 h-5 cursor-pointer"
+                          />
+                          <label
+                            htmlFor="terms&condition"
+                            className="cursor-pointer"
+                          >
+                            Agree to Terms & Conditions
+                          </label>
+                        </div>
+                        <button
+                          className="btn-primary max-sm:!w-full"
+                          type="submit"
                         >
-                          Agree to Terms & Conditions
-                        </label>
+                          Book Catering Now
+                        </button>
                       </div>
-                      <button
-                        className="btn-primary max-sm:!w-full"
-                        type="submit"
-                      >
-                        Book Catering Now
-                      </button>
                     </div>
                   </div>
-                </div>
-              </form>
+                </form>
+              </div>
+              <div className="w-2/5">
+                <Image
+                  draggable={false}
+                  alt="Booking"
+                  src={"/img/booking-form.png"}
+                  width={400}
+                  height={400}
+                  className="w-full h-auto"
+                />
+              </div>
             </div>
           </div>
         </div>
